42220d720395c29b276fb52e0c53a307e290ce6b
Ryan <ryan.enslow@gmail.com>
2020-10-03 16:20:32 -0700

    Started work on adding listeners in the MainActivity.java file.

--- app/src/main/java/com/example/enslowfacemaker/Face.java
+++ app/src/main/java/com/example/enslowfacemaker/Face.java
@@ -3,22 +3,36 @@
 
 
 package com.example.enslowfacemaker;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+import android.view.View;
+
 import java.lang.Math;
 
 //This class contains the necessary variables and methods for the face
-public class Face {
+public class Face extends SurfaceView {
     private int skinColor;
     private int eyeColor;
     private int hairColor;
     private int hairStyle;
 
     //Constructor for Face Object sets values of all instance variables to random values.
-    public Face(){
+    public Face(Context context, AttributeSet attrs){
+        super(context, attrs);
+        setWillNotDraw(false);
         this.skinColor = randomize(255.0);
         this.eyeColor = randomize(255.0);
         this.hairColor = randomize(255.0);
         this.hairStyle = randomize(3.0);
     }
+
+    @Override
+    public void onDraw(Canvas c){
+
+    }
+
     //This method takes in a double called x and returns a random integer from [0,x]
     public int randomize(double max){
         return (int) (Math.random()*(max));
--- app/src/main/java/com/example/enslowfacemaker/MainActivity.java
+++ app/src/main/java/com/example/enslowfacemaker/MainActivity.java
@@ -4,26 +4,49 @@ package com.example.enslowfacemaker;
 
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.net.sip.SipSession;
+import android.os.Bundle;
+import android.view.SurfaceView;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 
 //This is where the main method will be run from. It is empty for now until I work on Part B
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(R.layout.activity_main);
+
+    }
 
 
+    //This is the helper method that will populate the Spinner
+    public void populateSpinner() {
+        //This block of code finds the spinner and then populates it with items in an array located in the resource folder. I learned how to do this from the Android Studio Documentation for spinners
+        //https://developer.android.com/guide/topics/ui/controls/spinner#java
+        Spinner hairSpinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> a = ArrayAdapter.createFromResource(this, R.array.hair_array, android.R.layout.simple_spinner_item);
+        a.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        hairSpinner.setAdapter(a);
+        hairSpinner.setOnItemSelectedListener(this);
+    }
 
-}
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
 
-//This is the method that will populate the Spinner
-public void populateSpinner()
-{
-    //This block of code finds the spinner and then populates it with items in an array located in the resource folder. I learned how to do this from the Android Studio Documentation for spinners
-    //https://developer.android.com/guide/topics/ui/controls/spinner#java
-    Spinner hairSpinner = (Spinner) findViewById(R.id.spinner);
-    ArrayAdapter<CharSequence> a = ArrayAdapter.createFromResource(this,R.array.hair_array,android.R.layout.simple_spinner_item);
-    a.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-    hairSpinner.setAdapter(a);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
 }
