343ed27e8a9274477554ed10b13d5789502d9720
Ryan <ryan.enslow@gmail.com>
2020-10-04 23:53:11 -0700

    Final Copy of the Homework. Everything is included

--- app/src/main/java/com/example/enslowfacemaker/Face.java
+++ app/src/main/java/com/example/enslowfacemaker/Face.java
@@ -10,9 +10,8 @@ import android.graphics.Paint;
 import android.graphics.RectF;
 import android.os.Build;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.SurfaceView;
-import android.view.View;
+import android.graphics.Path;
 
 import androidx.annotation.RequiresApi;
 
@@ -20,10 +19,10 @@ import java.lang.Math;
 
 //This class contains the necessary variables and methods for the face
 public class Face extends SurfaceView {
-    private int skinColor;
-    private int eyeColor;
-    private int hairColor;
-    private int hairStyle;
+    public int skinColor;
+    public int eyeColor;
+    public int hairColor;
+    public int hairStyle;
 
     Paint skinPaint = new Paint();
     Paint eyePaint = new Paint();
@@ -32,57 +31,59 @@ public class Face extends SurfaceView {
     final int Marginx = 10;
     final int Marginy = 10;
 
-
-
     //Constructor for Face Object sets values of all instance variables to random values.
     public Face(Context context, AttributeSet attrs){
         super(context, attrs);
         setWillNotDraw(false);
 
-        //any constants will go here
-
-
+        //Initialize colors to be random
         this.skinColor = randomize(0xFFFFFF)+(0xFF000000);
         this.eyeColor = randomize(0xFFFFFF)+(0xFF000000);
         this.hairColor = randomize(0xFFFFFF)+(0xFF000000);
-        this.hairStyle = randomize(3);
+        this.hairStyle = randomize(2);
 
-        skinPaint.setColor(this.skinColor);
-        eyePaint.setColor(this.eyeColor);
-        hairPaint.setColor(this.hairColor);
+        //other variables to initialize mainly for preference.
         skinPaint.setStyle(Paint.Style.FILL);
         eyePaint.setStyle(Paint.Style.FILL);
         hairPaint.setStyle(Paint.Style.FILL);
-
         setBackgroundColor(Color.BLACK);
     }
 
+    //Don't know what this is but android studio really wanted it
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+
+    //On draw method is called whenever invalidate is called on an instance of this class
     @Override
     public void onDraw(Canvas c){
+        //these colors are set again so that the colors can be changed via user interaction.
+        skinPaint.setColor(this.skinColor);
+        eyePaint.setColor(this.eyeColor);
+        hairPaint.setColor(this.hairColor);
+
+        //Return the width of the view
         int w = getWidth()-Marginx;
         int h = getHeight()-Marginy;
+
+        //Draws the head
         int headW1 = w/8+Marginx;
         int headH1 = h/8-Marginy;
         int headW2 = 7*w/8;
         int headH2 = 7*h/8;
 
-        //draws the head
+        //draws the face
         c.drawOval(headW1,headH1,headW2,headH2,skinPaint);
         drawEyes(c,w,h);
         drawMouth(c,w,h);
-
-
+        drawHair(c,w,h,hairStyle);
     }
 
-    //not sure what this means entirely, but android studio really wanted it in there
     //helper method draws both eyes. Takes in the size of canvas as parameters
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public void drawEyes(Canvas c, int w, int h){
         //draw first eye
-        c.drawOval(w/4,h/4,3*w/8,h/2,eyePaint);
-        c.drawOval(w-w/4,h/4,w-3*w/8,h/2,eyePaint);
-
+        c.drawOval(w/4,h/3,3*w/8,h/2,eyePaint);
+        //draw second eye
+        c.drawOval(w-w/4,h/3,w-3*w/8,h/2,eyePaint);
     }
 
     //helper method draws mouth. Takes canvas size as parameters
@@ -91,22 +92,42 @@ public class Face extends SurfaceView {
         c.drawArc(box,0.0f,180.0f,false,eyePaint);
     }
 
-    //helper method to draw the hair. Takes canvas size and hairstyle as parameters
+    //helper method to draw the hair. Takes canvas size and hairstyle number as parameters
     public void drawHair(Canvas c, int w, int h, int hairStyle){
-        if (hairStyle == 1){
-
+        //basic hair style
+        if (hairStyle == 0){
+            RectF box = new RectF(w/4.9f,h/9,w-w/4.9f,3*h/7);
+            c.drawArc(box,180.0f,180.0f,false,hairPaint);
         }
-        else if(hairStyle == 2){
-
+        //hat hairstyle
+        else if(hairStyle == 1) {
+            RectF box = new RectF(w/4.5f,h/10,w-w/4.5f,3*h/8);
+            c.drawArc(box,180.0f,180.0f,false,hairPaint);
+            c.drawRect(w/10,h/4-h/20,w-w/10,3*h/10-h/20,hairPaint);
         }
-        else{
-
+        //triangle horns
+        else if(hairStyle == 2) {
+            dTriangle(c,w/4,h/5,w/10,0);
+            dTriangle(c,w-w/4,h/5,w/10,(float) Math.PI);
         }
 
     }
 
+    //helper method draws an equilateral triangle with center x,y with radius r and angle offset o
+    //This method is too complex for what I'm using it for, but it's fun to write so I did it
+    public void dTriangle(Canvas c, float x, float y, float r, float o){
+        Path t = new Path();
+        //This tells us that the angle difference between each point is 2pi/3
+        float s = (float) ((2.0*Math.PI)/3);
+        //This loop starts at some angle and then creates a point 2pi/3 radians away from the first until the last point is drawn
+        for (int i = 0; i < 4; i++) {
+            t.lineTo(x + (float) (r * Math.cos(s*i+o)), y + (float) (r * Math.sin(s*i+o)));
+        }
+        //This takes the path and draws it
+        c.drawPath(t,hairPaint);
+    }
     //This method takes in an int called x and returns a random integer from [0,x]
-    public int randomize(int max){
+    public static int randomize(int max){
         return (int) (Math.random()*max);
     }
 }
--- app/src/main/java/com/example/enslowfacemaker/MainActivity.java
+++ app/src/main/java/com/example/enslowfacemaker/MainActivity.java
@@ -1,8 +1,8 @@
 //@Ryan Enslow
 
 package com.example.enslowfacemaker;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,14 +11,23 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.Spinner;
-
 import androidx.appcompat.app.AppCompatActivity;
+import static com.example.enslowfacemaker.Face.randomize;
 
 
 //This is where the main method will be run from. It is empty for now until I work on Part B
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener, SeekBar.OnSeekBarChangeListener, RadioGroup.OnCheckedChangeListener {
 
     private Face faceView;
+    private int rSlide = 0;
+    private int gSlide = 0;
+    private int bSlide = 0;
+    private RadioButton hairSelect;
+    private RadioButton skinSelect;
+    private RadioButton eyesSelect;
+    private SeekBar redBar;
+    private SeekBar greenBar;
+    private SeekBar blueBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +37,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         faceView = findViewById(R.id.face);
         //See helper method description
         initializeListeners();
-
-
     }
+
 
     //Helper method to initialize all fo the listeners in the View
     public void initializeListeners(){
-        //populateSpinner();
+        populateSpinner();
 
         Button rFace = findViewById(R.id.randFace);
         rFace.setOnClickListener(this);
@@ -42,26 +50,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         RadioGroup groupSelect = findViewById(R.id.radioGroup);
         groupSelect.setOnCheckedChangeListener(this);
 
-        RadioButton hairSelect = findViewById(R.id.radioHair);
-        hairSelect.setOnClickListener(this);
+        this.hairSelect = findViewById(R.id.radioHair);
+        this.hairSelect.setOnClickListener(this);
 
-        RadioButton eyesSelect = findViewById(R.id.radioEyes);
-        eyesSelect.setOnClickListener(this);
+        this.eyesSelect = findViewById(R.id.radioEyes);
+        this.eyesSelect.setOnClickListener(this);
 
-        RadioButton skinSelect = findViewById(R.id.radioSkin);
-        skinSelect.setOnClickListener(this);
+        this.skinSelect = findViewById(R.id.radioSkin);
+        this.skinSelect.setOnClickListener(this);
 
-        SeekBar redBar = findViewById(R.id.redBar);
-        redBar.setOnSeekBarChangeListener(this);
+        this.redBar = findViewById(R.id.redBar);
+        this.redBar.setOnSeekBarChangeListener(this);
 
-        SeekBar greenBar = findViewById(R.id.greenBar);
-        greenBar.setOnSeekBarChangeListener(this);
+        this.greenBar = findViewById(R.id.greenBar);
+        this.greenBar.setOnSeekBarChangeListener(this);
+
+        this.blueBar = findViewById(R.id.blueBar);
+        this.blueBar.setOnSeekBarChangeListener(this);
     }
 
     //This is the helper method that will populate the Spinner
     public void populateSpinner() {
-        //This block of code finds the spinner and then populates it with items in an array located in the resource folder. I learned how to do this from the Android Studio Documentation for spinners
-        //https://developer.android.com/guide/topics/ui/controls/spinner#java
+        /**
+        External Citation:
+        Problem: Didn't know how to make a spinner work
+        This block of code finds the spinner and then populates it with items in an array located in the resource folder. I learned how to do this from the Android Studio Documentation for spinners
+        https://developer.android.com/guide/topics/ui/controls/spinner#java
+         */
         Spinner hairSpinner = (Spinner) findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> a = ArrayAdapter.createFromResource(this, R.array.hair_array, android.R.layout.simple_spinner_item);
         a.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -69,14 +84,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         hairSpinner.setOnItemSelectedListener(this);
     }
 
+    //listener to check if new hair has been selected
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-
+        faceView.hairStyle = i;
+        faceView.invalidate();
     }
 
+    //This method doesn't do anything, but I'm too scared to delete it
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
-
+        faceView.hairStyle = 0;
+        faceView.invalidate();
     }
 
     @Override
@@ -84,24 +103,77 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
+    //This line helps me sleep at night but it's basically useless
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
-
+        faceView.invalidate();
     }
 
+
+    //This method is called whenever a seekbar is moved.
+    //This controls updating the color of the drawings on screen
     @Override
     public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        //Determines which bar is moved and sets a value to the progress corresponding to the bar.
+        //Then it updates the color
+        if (seekBar.getId()==R.id.redBar){ rSlide = i; }
+        else if (seekBar.getId()==R.id.greenBar){ gSlide = i; }
+        else{ bSlide = i; }
 
+        int c = Color.rgb(rSlide,gSlide,bSlide);
+        if (this.hairSelect.isChecked()){ faceView.hairColor = c; }
+        else if (this.eyesSelect.isChecked()){ faceView.eyeColor = c; }
+        else if (this.skinSelect.isChecked()){ faceView.skinColor = c; }
+        faceView.invalidate();
     }
 
+    //This onClick method controls the radioButtons and the Random Button
     @Override
     public void onClick(View view) {
-        Log.d("onClick", "onClick: yoooo");
-        view.invalidate();
-    }
+        int r;
+        int g;
+        int b;
+        //Controls random button
+        if (view.getId() == R.id.randFace) {
+            faceView.skinColor = randomize(0xFFFFFF) + (0xFF000000);
+            faceView.eyeColor = randomize(0xFFFFFF) + (0xFF000000);
+            faceView.hairColor = randomize(0xFFFFFF) + (0xFF000000);
+            faceView.hairStyle = randomize(3);
+            RadioGroup n = findViewById(R.id.radioGroup);
+            n.clearCheck();
+        }
 
+        //Controls all three seekbars
+        //Set seekbar back to values it had
+        //I really felt like I was able to shorten this code down, but it made things worse so I reverted it back im sorry
+        else if (view.getId() == R.id.radioHair){
+            r = Color.red(faceView.hairColor);
+            g = Color.green(faceView.hairColor);
+            b = Color.blue(faceView.hairColor);
+            this.redBar.setProgress(r);
+            this.greenBar.setProgress(g);
+            this.blueBar.setProgress(b);
+        }
+        else if (view.getId() == R.id.radioEyes){
+            r = Color.red(faceView.eyeColor);
+            g = Color.green(faceView.eyeColor);
+            b = Color.blue(faceView.eyeColor);
+            this.redBar.setProgress(r);
+            this.greenBar.setProgress(g);
+            this.blueBar.setProgress(b);
+        }
+        else if (view.getId() == R.id.radioSkin){
+            r = Color.red(faceView.skinColor);
+            g = Color.green(faceView.skinColor);
+            b = Color.blue(faceView.skinColor);
+            this.redBar.setProgress(r);
+            this.greenBar.setProgress(g);
+            this.blueBar.setProgress(b);
+        }
+        faceView.invalidate();
+    }
     @Override
     public void onCheckedChanged(RadioGroup radioGroup, int i) {
-
     }
+
 }
